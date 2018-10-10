c8701a25f5a8fd3c01fc7cfa562cd92bd43a6ad1
Daniël Acohen <daan.acohen@connectingapps.net>
2018-10-10 19:12:28 +0200

    Added code of poli demo

--- PauliDemo/Driver.cs
+++ PauliDemo/Driver.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Quantum.Simulation.Core;
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.Simulation.Simulators;
 
 namespace Quantum.PauliDemo
@@ -7,7 +8,32 @@ namespace Quantum.PauliDemo
     {
         static void Main(string[] args)
         {
+            Result[] inputs = { Result.Zero, Result.One };
 
+            using (var sim = new QuantumSimulator())
+            {
+                Console.WriteLine("First use single X operations");
+                Console.WriteLine("-----------------------------");
+                foreach (var input in inputs)
+                {
+                    for (int i = 1; i < 10; i++)
+                    {
+                        var result = ExecuteXOperations.Run(sim, i, input, false).Result;
+                        Console.WriteLine($"Number of X operations is {i}. The input is {input}. The output is {result}");
+                    }
+                }
+                Console.WriteLine("Now use double X operations");
+                Console.WriteLine("-----------------------------");
+                foreach (var input in inputs)
+                {
+                    for (int i = 1; i < 10; i++)
+                    {
+                        var result = ExecuteXOperations.Run(sim, i, input, true).Result;
+                        Console.WriteLine($"Number of double X operations is {i}. The input is {input}. The output is {result}");
+                    }
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
--- PauliDemo/Operation.qs
+++ PauliDemo/Operation.qs
@@ -2,12 +2,71 @@
 {
     open Microsoft.Quantum.Primitive;
     open Microsoft.Quantum.Canon;
-
-    operation Operation () : ()
+	
+	operation ExecuteXOperations (numberOfTimes : Int, initialValue : Result, duplicate : Bool) : Result
     {
         body
         {
-            
+			mutable resultValue = initialValue;
+			using (register = Qubit[1])
+			{
+				SetValue(initialValue,register[0]);
+				if (duplicate)
+				{
+					let duplicateAction = CreateDoubleExecution(X);
+					DoSoManyTimes(numberOfTimes,register[0],duplicateAction);
+				}
+				else 
+				{
+					DoSoManyTimes(numberOfTimes,register[0],X);
+				}				
+				set resultValue = M(register[0]);
+				Reset(register[0]);
+			}
+            return resultValue;
         }
     }
+
+	operation ApplyOften(op : ((Qubit) => ()), numberOfTimes : Int,  target : Qubit) : () 
+	{
+		body 
+		{
+			for (i in 1..numberOfTimes)
+			{
+				op(target);
+			}
+		}
+	}
+
+	operation CreateDoubleExecution(op : ((Qubit) => ())) : ((Qubit) => ())
+	{
+		body
+		{
+			return ApplyOften(op,2,_);
+		}
+	}
+	
+    operation SetValue (desired: Result, q1: Qubit) : ()
+    {
+        body
+        {
+		    let current = M(q1);
+
+            if (desired != current)
+            {
+                X(q1);
+            }        
+        }
+	}
+
+	operation DoSoManyTimes(numberOfTimes : Int, target : Qubit, op : ((Qubit) => ())) : ()
+	{
+		body
+		{	
+			for (i in 1..numberOfTimes)
+			{
+				op(target);
+			}	
+		}
+	}
 }
