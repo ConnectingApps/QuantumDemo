0f6e4c7205afb6ac7a033ae59cde43ca83a90eb7
Daniël Acohen <daan.acohen@connectingapps.net>
2018-10-11 15:36:18 +0200

    Try CNOT, initial project. Functionality without hadamard shown as here:

---
+++ TryCNOT/Driver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.TryCNOT
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("firstQbit\tsecondQbit\tdoHamamardOnFirst\tdoCnot\tfirstResult\tsecondResult");
+            using (var sim = new QuantumSimulator())
+            {
+                (Result firstQbit, Result secondQbit, bool doHadamardOnFirst, bool doCnot) = (Result.Zero, Result.One,false,false);
+                var (firstResult, secondResult) = TryTheCNOT.Run(sim, firstQbit, secondQbit, doHadamardOnFirst, doCnot).Result;
+                Console.WriteLine($"{firstQbit}\t{secondQbit}\t{doHadamardOnFirst}\t{doCnot}\t{firstResult}\t{secondResult}");
+
+                (firstQbit, secondQbit,  doHadamardOnFirst,  doCnot) = (Result.Zero, Result.Zero, false, false);
+                (firstResult, secondResult) = TryTheCNOT.Run(sim, firstQbit, secondQbit, doHadamardOnFirst, doCnot).Result;
+                Console.WriteLine($"{firstQbit}\t{secondQbit}\t{doHadamardOnFirst}\t{doCnot}\t{firstResult}\t{secondResult}");
+
+                (firstQbit, secondQbit, doHadamardOnFirst, doCnot) = (Result.One, Result.Zero, false, false);
+                (firstResult, secondResult) = TryTheCNOT.Run(sim, firstQbit, secondQbit, doHadamardOnFirst, doCnot).Result;
+                Console.WriteLine($"{firstQbit}\t{secondQbit}\t{doHadamardOnFirst}\t{doCnot}\t{firstResult}\t{secondResult}");
+
+                (firstQbit, secondQbit, doHadamardOnFirst, doCnot) = (Result.One, Result.One, false, false);
+                (firstResult, secondResult) = TryTheCNOT.Run(sim, firstQbit, secondQbit, doHadamardOnFirst, doCnot).Result;
+                Console.WriteLine($"{firstQbit}\t{secondQbit}\t{doHadamardOnFirst}\t{doCnot}\t{firstResult}\t{secondResult}");
+
+                (firstQbit, secondQbit, doHadamardOnFirst, doCnot) = (Result.Zero, Result.Zero, false, true);
+                (firstResult, secondResult) = TryTheCNOT.Run(sim, firstQbit, secondQbit, doHadamardOnFirst, doCnot).Result;
+                Console.WriteLine($"{firstQbit}\t{secondQbit}\t{doHadamardOnFirst}\t{doCnot}\t{firstResult}\t{secondResult}");
+
+                (firstQbit, secondQbit, doHadamardOnFirst, doCnot) = (Result.One, Result.Zero, false, true);
+                (firstResult, secondResult) = TryTheCNOT.Run(sim, firstQbit, secondQbit, doHadamardOnFirst, doCnot).Result;
+                Console.WriteLine($"{firstQbit}\t{secondQbit}\t{doHadamardOnFirst}\t{doCnot}\t{firstResult}\t{secondResult}");
+
+                (firstQbit, secondQbit, doHadamardOnFirst, doCnot) = (Result.Zero, Result.One, false, true);
+                (firstResult, secondResult) = TryTheCNOT.Run(sim, firstQbit, secondQbit, doHadamardOnFirst, doCnot).Result;
+                Console.WriteLine($"{firstQbit}\t{secondQbit}\t{doHadamardOnFirst}\t{doCnot}\t{firstResult}\t{secondResult}");
+
+                (firstQbit, secondQbit, doHadamardOnFirst, doCnot) = (Result.One, Result.One, false, true);
+                (firstResult, secondResult) = TryTheCNOT.Run(sim, firstQbit, secondQbit, doHadamardOnFirst, doCnot).Result;
+                Console.WriteLine($"{firstQbit}\t{secondQbit}\t{doHadamardOnFirst}\t{doCnot}\t{firstResult}\t{secondResult}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TryCNOT/Operation.qs
@@ -0,0 +1,45 @@
+﻿namespace Quantum.TryCNOT
+{
+    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Canon;
+
+    operation TryTheCNOT (firstValue : Result, secondValue : Result, doHadamardOnFirst : Bool, doCNOT : Bool) : (Result,Result)
+    {
+        body
+        {
+			mutable firstResult = Zero;
+			mutable secondResult = Zero;
+			using (register = Qubit[2])
+			{
+				SetValue(firstValue,register[0]);
+				SetValue(secondValue,register[1]);
+				if (doHadamardOnFirst)
+				{
+					H(register[0]);				
+				}
+				if (doCNOT)
+				{
+					CNOT(register[0],register[1]);
+				}
+				set firstResult = M(register[0]);
+				set secondResult = M(register[1]);
+				Reset(register[0]);
+				Reset(register[1]);
+			}		
+            return (firstResult,secondResult);
+        }
+    }
+
+	operation SetValue (desired: Result, q1: Qubit) : ()
+    {
+        body
+        {
+		    let current = M(q1);
+
+            if (desired != current)
+            {
+                X(q1);
+            }        
+        }
+	}
+}
